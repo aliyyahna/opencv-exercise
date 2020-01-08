38d69be55969fa54180a8070caddf756c96549be
aliyyah <aliyyah1203@hotmail.com>
2020-01-08 18:46:53 +0800

    added files

--- chapter 4/grayscale.py
+++ chapter 4/grayscale.py
@@ -1,6 +1,6 @@
 import cv2;
 
-citra = cv2.imread('bear.jpg', cv2.IMREAD_GRAYSCALE)
+citra = cv2.imread('./img/baboon.png', cv2.IMREAD_GRAYSCALE)
 if not citra is None:
     cv2.imshow('Image', citra)
     cv2.waitKey(0)
---
+++ chapter 4/jenisgbr.py
@@ -0,0 +1,17 @@
+import cv2
+import sys
+
+if len(sys.argv) == 1:
+    print('Masukkan nama berkas gambar')
+else:
+    berkas = sys.argv[1]
+
+citra = cv2.imread(berkas, cv2.IMREAD_UNCHANGED)
+if citra is None:
+    print('Tidak dapat membaca berkas', berkas)
+else:
+    info = citra.shape
+    if len(info) == 2:
+        print('Citra berskala keabu-abuan')
+    else:
+        print('Citra berwarna')
