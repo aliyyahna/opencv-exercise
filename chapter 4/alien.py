d2d09c983084cd5a65a1b8f1a94a17961eb35476
aliyyah <aliyyah1203@hotmail.com>
2020-01-14 23:13:54 +0800

    added file

---
+++ chapter 4/alien.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+# buat citra berwarna hitam
+citra = np.zeros((256, 256, 3), np.uint8)
+
+# buat elips
+# buat busur
+# buat mata
+# buat belalai
+
+cv2.imshow('Hasil', citra)
+cv2.waitKey(0)
