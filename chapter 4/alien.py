a3a4ccaf6a1961fc71b659fd730fbe3303a8e353
aliyyah <aliyyah1203@hotmail.com>
2020-01-14 23:21:29 +0800

    update alien.py

--- chapter 4/alien.py
+++ chapter 4/alien.py
@@ -5,9 +5,17 @@ import numpy as np
 citra = np.zeros((256, 256, 3), np.uint8)
 
 # buat elips
+cv2.ellipse(citra, (128, 128), (100, 50), 0, 0, 360, (255, 255, 255), 5)
+
 # buat busur
+cv2.ellipse(citra(128, 230), (80, 50), 0, -180, 0, (255,255,255), 5)
+
 # buat mata
+cv2.circle(citra, (128, 128), 10, (0, 0, 128), 3)
+
 # buat belalai
+cv2.line(citra, (128, 80), (128, 40), (255, 255, 255), 3)
+cv2.circle(citra, (128, 45), 5, (255, 255, 255), 3)
 
 cv2.imshow('Hasil', citra)
 cv2.waitKey(0)
