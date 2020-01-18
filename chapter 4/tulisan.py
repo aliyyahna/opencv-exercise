b8df55903d17a7b00d470cb568e7ce12034e6e0e
aliyyah <aliyyah1203@hotmail.com>
2020-01-18 15:02:36 +0800

    added files

---
+++ chapter 4/tulisan.py
@@ -0,0 +1,10 @@
+import cv2
+
+citra = cv2.imread('./img/baboon.png')
+
+# buat tulisan
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(citra, 'Baboon', (10, 500), font, 4, (0, 0, 0), 10)
+
+cv2.imshow('Hasil', citra)
+cv2.waitKey(0)
